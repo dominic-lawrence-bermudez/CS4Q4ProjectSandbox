668d04094486d5cfc527f3c941388afa4630d0b7
lwrnc788 <37734971+lwrnc788@users.noreply.github.com>
2020-03-25 07:45:36 +0800

    (failed) attempt 1 to implement Player movement

--- src/cs4q4projectsandbox/CS4Q4ProjectSandboxController.java
+++ src/cs4q4projectsandbox/CS4Q4ProjectSandboxController.java
@@ -3,6 +3,9 @@ package cs4q4projectsandbox;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.animation.TranslateTransition;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.image.Image;
@@ -10,40 +13,35 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
 
 public class CS4Q4ProjectSandboxController implements Initializable {
+    @FXML AnchorPane ap_parentAnchor;
+    
+    final private int[] GridSpawnpoint = {9, 7};    
+    int currentMapFloor;
+    int[] currentMapXY;
+    
+    private Player player;
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        ap_parentAnchor.requestFocus();
+                
+        player = new Player(GridSpawnpoint);
+        updatePlayerLocation();
+        
+        //---
+        
         currentMapFloor = 1;
         currentMapXY = new int[]{3, 2};
         
         loadMap(currentMapFloor, currentMapXY);
-        
-        //---
-        
-        player = new Player();
-        
-        ap_parentAnchor.requestFocus();
     }
-    
-    @FXML AnchorPane ap_parentAnchor;
-    
-    int currentMapFloor;
-    int[] currentMapXY;
-    
-    Player player;
     
     //---
     
     @FXML ImageView imgv_mapImage;
-    
-    void changeMapToAdjacent(int dF, int dX, int dY) {
-        currentMapFloor += dF;
-        currentMapXY[0] += dX;
-        currentMapXY[1] += dY;
-        
-        loadMap(currentMapFloor, currentMapXY);
-    }
     
     void loadMap(int cMF, int[] cMXY) {
         Map currentMap = new Map(currentMapFloor, currentMapXY);
@@ -52,41 +50,67 @@ public class CS4Q4ProjectSandboxController implements Initializable {
         imgv_mapImage.setImage(new Image(MapImagePath));
     }
     
+    void changeMapToAdjacent(String relativeDirection) {
+        int dF = 0, dX = 0, dY = 0;
+        
+        switch (relativeDirection) {
+            case "below":
+                dF = -1;
+                break;
+            case "above":
+                dF = +1;
+                break;
+            case "left":
+                dX = -1;
+                break;
+            case "right":
+                dX = +1;
+                break;
+            case "up":
+                dY = -1;
+                break;
+            case "down":
+                dY = +1;
+                break;
+            default:
+                break;
+        }
+        
+        currentMapFloor += dF;
+        currentMapXY[0] += dX;
+        currentMapXY[1] += dY;
+        
+        loadMap(currentMapFloor, currentMapXY);
+    }
+    
+    //---
+    
     @FXML ImageView imgv_playerImage;
     
-    @FXML
-    void clicky() {
-        System.out.println("test");
+    void updatePlayerLocation() {
+        imgv_playerImage.setLayoutX(player.getPixelPosition()[0]);
+        imgv_playerImage.setLayoutY(player.getPixelPosition()[1]);
     }
+    
+    //---
+    
+    String[] PlayerMovement_XY = new String[2];
     
     @FXML
     void detectKeyPress(KeyEvent ke) throws IOException {
         KeyCode KeyEventCode = ke.getCode();
-        
+              
         switch (KeyEventCode) {
             case LEFT:
-                player.setDirectionFacing("left");
-                player.walk();
-                //imgv_playerImage.setLayoutX(player.PlayerPosition[0]);
-                break;
             case RIGHT:
-                player.setDirectionFacing("right");
-                player.walk();
-                //imgv_playerImage.setLayoutX(player.PlayerPosition[0]);
-                break;
-            case DOWN:
-                player.setDirectionFacing("down");
-                player.walk();
-                //imgv_playerImage.setLayoutY(player.PlayerPosition[1]);
-                break;
             case UP:
-                player.setDirectionFacing("up");
-                player.walk();
-                //imgv_playerImage.setLayoutY(player.PlayerPosition[1]);
+            case DOWN:
+                moveObjectSmoothly(imgv_playerImage, KeyEventCode);
                 break;
                 
             case X:
                 //openInventory()'
+                changeMapToAdjacent("left");
                 break;
             
             case ESCAPE:
@@ -117,5 +141,28 @@ public class CS4Q4ProjectSandboxController implements Initializable {
         }
         
         System.out.println("-" + player.getDirectionFacing());
+    }
+    
+    //---
+    
+    void moveObjectSmoothly (ImageView imageToMove, KeyCode direction) {
+        TranslateTransition tt = new TranslateTransition(Duration.millis(200), imageToMove);
+        
+        switch (direction) {
+            case LEFT:
+                tt.setByX(-Tile.Size);
+                break;
+            case RIGHT:
+                tt.setByX(Tile.Size);
+                break;
+            case UP:
+                tt.setByY(-Tile.Size);
+                break;
+            case DOWN:
+                tt.setByY(Tile.Size);
+                break;
+        }
+        
+        tt.play();
     }
 }
--- src/cs4q4projectsandbox/obj_Player.java
+++ src/cs4q4projectsandbox/obj_Player.java
@@ -4,37 +4,58 @@ class Player {
     final String FullName = "Jerry Barry Cruz";
     final String Nickname = "Jeby";
     
-    int[] PlayerPosition_Grid; //int[2]
-    int[] PlayerPosition_Pixel;
+    //---
+    
+    int[] GridPosition = new int[2];
+    int[] PixelPosition = new int[2];
     private String DirectionFacing;
     
-    Player() {
+    Player(int[] GP) {
         DirectionFacing = "down";
-        PlayerPosition_Grid = new int[]{9, 7};
+        
+        GridPosition[0] = GP[0];
+        GridPosition[1] = GP[1];
+        
+        updatePixelPosition();
     }
     
     void walk() {
         switch (DirectionFacing) {
             case "left":
-                PlayerPosition_Grid[0] -= 1;
+                GridPosition[0]--;
                 break;
             case "right":
-                PlayerPosition_Grid[0] += 1;
-                break;
-            case "down":
-                PlayerPosition_Grid[1] -= 1;
+                GridPosition[0]++;
                 break;
             case "up":
-                PlayerPosition_Grid[1] += 1;
+                GridPosition[1]--;
+                break;
+            case "down":
+                GridPosition[1]++;
                 break;
         }
+        
+        updatePixelPosition();
+    }
+    
+    //---
+    
+    private void updatePixelPosition() {
+        PixelPosition[0] = Tile.Size * GridPosition[0];
+        PixelPosition[1] = Tile.Size * GridPosition[1];
+    }
+    
+    int[] getPixelPosition() {
+        return PixelPosition;
+    }
+    
+    //---
+    
+    void setDirectionFacing(String ND) {
+        DirectionFacing = ND;
     }
     
     String getDirectionFacing() {
         return DirectionFacing;
-    }
-    
-    void setDirectionFacing(String ND) {
-        DirectionFacing = ND;
     }
 }
