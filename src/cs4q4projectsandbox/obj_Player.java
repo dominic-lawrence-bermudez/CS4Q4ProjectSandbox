9d9c5f55a0c4a71f2abb04432a198cf89a7923e4
lwrnc788 <37734971+lwrnc788@users.noreply.github.com>
2020-03-28 05:53:12 +0800

    added more references to Mapp

--- src/cs4q4projectsandbox/obj_Player.java
+++ src/cs4q4projectsandbox/obj_Player.java
@@ -1,12 +1,12 @@
 package cs4q4projectsandbox;
 
 class Player {
-    final private int TileSize = 64;
-    
     final String FullName = "Jerry Barry Cruz";
     final String Nickname = "Jeby";
     
     //---
+    
+    Mapp ContainingMapp;
     
     int[] GridPosition = new int[2];
     int[] PixelPosition = new int[2];
@@ -43,26 +43,54 @@ class Player {
     
     //---
     
+    void setGridPosition(int GPx, int GPy) {
+        this.GridPosition[0] = GPx;
+        this.GridPosition[1] = GPy;
+    }
+    
     private void updatePixelPosition() {
-        PixelPosition[0] = TileSize * GridPosition[0];
-        PixelPosition[1] = TileSize * GridPosition[1];
+        this.PixelPosition[0] = Mapp.TILE_SIZE * this.GridPosition[0];
+        this.PixelPosition[1] = Mapp.TILE_SIZE * this.GridPosition[1];
+    }
+    
+    int[] getGridPosition() {
+        return this.GridPosition;
     }
     
     int[] getPixelPosition() {
-        return PixelPosition;
+        return this.PixelPosition;
     }
     
     //---
     
-    void setDirectionFacing(String ND) {
-        DirectionFacing = ND;
+    void setDirectionFacing(String DF) {
+        this.DirectionFacing = DF;
     }
     
     String getDirectionFacing() {
-        return DirectionFacing;
+        return this.DirectionFacing;
     }  
     
     //---
     
-    
+    MappObject getObjectInDirection(Mapp ContainingMapp, String Direction) {
+        int dX = 0, dY = 0;
+        
+        switch (Direction) {
+            case "left":
+                dX = -1;
+                break;
+            case "right":
+                dX = +1;
+                break;
+            case "up":
+                dY = -1;
+                break;
+            case "down":
+                dY = +1;
+                break;
+        }
+        
+        return ContainingMapp.getMappObject(this.GridPosition[0] + dX, this.GridPosition[1] + dY);
+    }
 }
