01d3c6bd7d65f1d5b0ee7209870f8baf7e7cbfa4
lwrnc788 <37734971+lwrnc788@users.noreply.github.com>
2020-03-28 05:52:16 +0800

    updated class references; changed walking type to "instantly appearing"

--- src/cs4q4projectsandbox/CS4Q4ProjectSandboxController.java
+++ src/cs4q4projectsandbox/CS4Q4ProjectSandboxController.java
@@ -5,19 +5,16 @@ import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.ResourceBundle;
-import javafx.animation.AnimationTimer;
-import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
-import javafx.util.Duration;
+import javafx.scene.layout.StackPane;
 
 public class CS4Q4ProjectSandboxController implements Initializable {
     @FXML AnchorPane ap_parentAnchor;
@@ -25,6 +22,7 @@ public class CS4Q4ProjectSandboxController implements Initializable {
     final private int[] GridSpawnpoint = {9, 7};    
     int currentMapFloor;
     int[] currentMapXY;
+    Mapp currentMapp;
     
     private Player player;
     
@@ -33,14 +31,19 @@ public class CS4Q4ProjectSandboxController implements Initializable {
         ap_parentAnchor.requestFocus();
                 
         player = new Player(GridSpawnpoint);
-        updatePlayerPosition();
+        updatePlayerPixelPosition();
         
         //---
         
         currentMapFloor = 1;
         currentMapXY = new int[]{2, 2};
         
+        changeMap(currentMapFloor, currentMapXY);
         loadMap(currentMapFloor, currentMapXY);
+    }
+    
+    void changeMap(int cMF, int[] cMXY) {
+        currentMapp = new Mapp(cMF, cMXY);
     }
     
     //---
@@ -48,9 +51,7 @@ public class CS4Q4ProjectSandboxController implements Initializable {
     @FXML ImageView imgv_mapImage;
     
     void loadMap(int cMF, int[] cMXY) {
-        Mapp currentMapp = new Mapp(currentMapFloor, currentMapXY);
-        
-        String MapImagePath = "file:./src/resources/maps/" + currentMapp.getMapImage() + ".png";
+        String MapImagePath = "file:./src/resources/maps/" + currentMapp.getMapID() + ".png";
         imgv_mapImage.setImage(new Image(MapImagePath));
     }
     
@@ -89,23 +90,39 @@ public class CS4Q4ProjectSandboxController implements Initializable {
     
     //---
     
+    //@FXML StackPane stkp_playerTile;
     @FXML ImageView imgv_playerImage;
     
-    void updatePlayerPosition() {
+    void updatePlayerPixelPosition() {
         imgv_playerImage.setLayoutX(player.getPixelPosition()[0]);
         imgv_playerImage.setLayoutY(player.getPixelPosition()[1]);
     }
     
-    void updateObjectPosition(MappObject mapObject, ImageView mapObjectImage) {
+    void updateObjectGridPosition(MappObject mapObject, ImageView mapObjectImage) {
         mapObjectImage.setLayoutX(mapObject.getPixelPosition()[0]);
         mapObjectImage.setLayoutY(mapObject.getPixelPosition()[1]);
     }
     
-    //---
+    boolean isPlayerMovableInDirection(String direction) {
+        MappObject overlap = player.getObjectInDirection(currentMapp, direction);
+        
+        return isObjectMovableInDirection(overlap, direction);
+    }
     
-    int[] PlayerMovement_XY = new int[2];
+    boolean isObjectMovableInDirection(MappObject objectToMove, String direction) {
+        MappObject overlap = objectToMove.getObjectInDirection(objectToMove.getContainingMapp(), direction);
+        
+        switch (overlap.getCollisionType()) {
+            case "wall":
+                return false;
+            case "push":
+                return isObjectMovableInDirection(overlap, direction);
+            default:
+                return true;
+        }
+    }
     
-    Map<Integer, Integer> TestTest = new HashMap<>();
+    //--- Key Listeners
     
     @FXML
     void detectKeyPress(KeyEvent ke) throws IOException {
@@ -113,16 +130,24 @@ public class CS4Q4ProjectSandboxController implements Initializable {
               
         switch (KeyEventCode) {
             case LEFT:
-                PlayerMovement_XY[0]--;
+                player.setDirectionFacing("left");
+                player.walk();
+                updatePlayerPixelPosition();
                 break;
             case RIGHT:
-                PlayerMovement_XY[0]++;
+                player.setDirectionFacing("right");
+                player.walk();
+                updatePlayerPixelPosition();
                 break;
             case UP:
-                PlayerMovement_XY[1]--;
+                player.setDirectionFacing("up");
+                player.walk();
+                updatePlayerPixelPosition();
                 break;
             case DOWN:
-                PlayerMovement_XY[1]++;
+                player.setDirectionFacing("down");
+                player.walk();
+                updatePlayerPixelPosition();
                 break;
                 
             case X:
@@ -147,16 +172,12 @@ public class CS4Q4ProjectSandboxController implements Initializable {
         
         switch (KeyEventCode) {
             case LEFT:
-                PlayerMovement_XY[0]++;
                 break;
             case RIGHT:
-                PlayerMovement_XY[0]--;
                 break;
             case UP:
-                PlayerMovement_XY[1]++;
                 break;
             case DOWN:
-                PlayerMovement_XY[1]--;
                 break;
                 
             default:
@@ -168,35 +189,4 @@ public class CS4Q4ProjectSandboxController implements Initializable {
     
     //---
     
-    void moveObjectSmoothly (ImageView imageToMove, KeyCode direction) {
-        /*
-        
-        TranslateTransition tt = new TranslateTransition(Duration.millis(200), imageToMove);
-        
-        switch (direction) {
-            case LEFT:
-                tt.setByX(-Tile.Size);
-                break;
-            case RIGHT:
-                tt.setByX(Tile.Size);
-                break;
-            case UP:
-                tt.setByY(-Tile.Size);
-                break;
-            case DOWN:
-                tt.setByY(Tile.Size);
-                break;
-        }
-
-        tt.play();
-        
-        */
-        
-        new AnimationTimer() {
-            @Override
-            public void handle(long t_now) {
-                Scene scene = imageToMove.getScene();
-            }
-        }.start();
-    } 
 }
