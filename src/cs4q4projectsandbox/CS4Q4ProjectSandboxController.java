7e53eef22eb6c3ea9c21f8a1a968b61af62442c6
lwrnc788 <37734971+lwrnc788@users.noreply.github.com>
2020-03-28 06:37:58 +0800

    added a filler scene change method

--- src/cs4q4projectsandbox/CS4Q4ProjectSandboxController.java
+++ src/cs4q4projectsandbox/CS4Q4ProjectSandboxController.java
@@ -5,8 +5,6 @@ import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -17,7 +15,6 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class CS4Q4ProjectSandboxController implements Initializable {
