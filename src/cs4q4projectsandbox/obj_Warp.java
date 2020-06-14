a146495e1e3de4437ab3480b6e46690cf904b1d1
dominic lawrence bermudez <>
2020-06-15 01:53:26 +0800

    !!! - added a Warp class for future use

---
+++ src/cs4q4projectsandbox/obj_Warp.java
@@ -0,0 +1,17 @@
+package cs4q4projectsandbox;
+
+/*
+    If reqs get finished then please remember to implement warp
+    and remove all instances of Floor. 3D array have a ton of empty
+    space and have almost caused this whole project to timeout and
+    not run. It's much more efficient to just have each floor be
+    rendered on its own. Forget realism; everything's bound to be
+    bigger on the inside. This isn't a long-term game project anyway.
+    Just make do with what works, and hope that you even finish it.
+
+    - 06/15/2020
+*/
+
+class Warp {
+    
+}
